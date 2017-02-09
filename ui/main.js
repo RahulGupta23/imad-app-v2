3650eab3a5b38f82e7bdb8fc152e87e924a4bfe1
RahulGupta23 <rrgupta2396@gmail.com>
2017-02-09 21:15:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,23 @@ var element=document.getElementById('main-text')
 element.innerHTML="Hello Rahul"
 
 
+var button=document.getElementById("counter");
+var counter=0;
+
+
+button.onclick=function(){
+    
+    //1
+    //2
+    //3
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML= counter.toString();
+    
+    
+    
+    
+}
+
+
+
