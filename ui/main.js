c1ecf43ccfb12e03e36ce1bf3e9fad167844e538
RahulGupta23 <rrgupta2396@gmail.com>
2017-02-09 20:40:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,16 +4,3 @@ var element=document.getElementById('main-text')
 element.innerHTML="Hello Rahul"
 
 
-var img=document.getElementById('madi')
-
-var marginLeft=0;
-function moveRight()
-{   
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-    
-}
-
-img.onclick= function() {
-    var interval=setInterval(moveRight,100);
-};
